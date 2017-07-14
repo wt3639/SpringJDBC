856d7e6476927dc05859a00e5a031c3c1f1e82eb
wt3639 <wutao3639@163.com>
2017-07-14 18:48:25 +0800

    spring-mybatis

--- src/main/java/com/tom/springjdbc/MainDemo.java
+++ src/main/java/com/tom/springjdbc/MainDemo.java
@@ -15,72 +15,86 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.tom.springjdbc.bean.Student;
 import com.tom.springjdbc.dao.StudentDAO;
 
-
-
 public class MainDemo {
-	  public static void main( String[] args ) throws IOException {
-		 /*
-		  * use mybatis
-		  */
-		  String resource = "mybatis-config.xml";
-		  InputStream inputStream = Resources.getResourceAsStream(resource);
-		  SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		  SqlSession session = sqlSessionFactory.openSession();
-		    	try {
-		    	  Student student1= (Student) session.selectOne("com.tom.springjdbc.studentMapper.selectStudent", 1L);
-		    	  System.out.println(student1);
-		    	  Student student = new Student();
-				  student.setName("lianlian");
-			      student.setSchool("xiada");
-		    	  session.insert("com.tom.springjdbc.studentMapper.insertStudent",student);
-		    	  session.delete("com.tom.springjdbc.studentMapper.deleteStudent", 24L);
-		    	  Student student2 = new Student();
-				  student2.setId(6L);
-				  student2.setName("Qii");
-		    	  session.update("com.tom.springjdbc.studentMapper.updateStudent",student2);
-		    	  List<Student> lS = null;
-				    lS = session.selectList("com.tom.springjdbc.studentMapper.selectAllStudent");
-				    Iterator iter = lS.iterator();
-				    while(iter.hasNext()){
-				    	System.out.println(iter.next());
-				    	
-				    }
-				    session.commit();
-		    	} finally {
-		    	  session.close();
-		    	}
-		  
-		    	
-		    	/*
-		    	 * use jdbcTemplate
-		    try {
-				
-		    	
-		    	
-					ApplicationContext context = new ClassPathXmlApplicationContext("spring-module.xml");
-					 
-				    StudentDAO studentDAO = (StudentDAO) context.getBean("studentDAO");
-				    Student student = new Student();
-				    student.setName("lucy");
-				    student.setSchool("beida");
-				    studentDAO.addStudent(student);
-				    Student student2 = new Student();
-				    student2.setId(5L);
-				    student2.setName("Qi");
-				    studentDAO.updateStudent(student2);
-				    studentDAO.delStudent(23L);
-				    Student student1 = studentDAO.get(1L);
-				    System.out.println(student1);
-				    List<Student> lS = null;
-				    lS = studentDAO.query();
-				    Iterator iter = lS.iterator();
-				    while(iter.hasNext()){
-				    	System.out.println(iter.next());
-				    	
-				    }
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-}
+	public static void main(String[] args) throws IOException {
+		/*
+		 * use mybatis
+		 */
+		/*
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession session = sqlSessionFactory.openSession();
+		*/
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-module.xml");				 
+		StudentDAO studentDAO = (StudentDAO) context.getBean("studentDAO");
+		try {
+			//StudentDAO studentDAO = session.getMapper(StudentDAO.class);
+			Student student1 = studentDAO.get(1L);
+			System.out.println(student1); 
+			Student student = new Student();
+			student.setName("lianlian");
+			student.setSchool("xiada");
+			studentDAO.addStudent(student);
+			studentDAO.delStudent(25L);
+			Student student2 = new Student();
+			student2.setId(6L);
+			student2.setName("Qii");
+			studentDAO.updateStudent(student2);
+			List<Student> lS = null;
+			lS = studentDAO.query();
+			Iterator<Student> iter = lS.iterator();
+			while (iter.hasNext()) {
+				System.out.println(iter.next());
+
+			}
+
+			/*
+			 * Student student1= (Student)
+			 * session.selectOne("com.tom.springjdbc.dao.StudentDAO.get", 1L);
+			 * System.out.println(student1); Student student = new Student();
+			 * student.setName("lianlian"); student.setSchool("xiada");
+			 * session.insert("com.tom.springjdbc.dao.StudentDAO.addStudent",
+			 * student);
+			 * session.delete("com.tom.springjdbc.dao.StudentDAO.delStudent",
+			 * 24L); Student student2 = new Student(); student2.setId(6L);
+			 * student2.setName("Qii");
+			 * session.update("com.tom.springjdbc.dao.StudentDAO.updateStudent",
+			 * student2); List<Student> lS = null; lS =
+			 * session.selectList("com.tom.springjdbc.dao.StudentDAO.query");
+			 * Iterator iter = lS.iterator(); while(iter.hasNext()){
+			 * System.out.println(iter.next());
+			 * 
+			 * }
+			 */
+			//session.commit();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			//session.close();
+		}
+
+		/*
+		 * use jdbcTemplate try {
+		 * 
+		 * 
+		 * 
+		 * ApplicationContext context = new
+		 * ClassPathXmlApplicationContext("spring-module.xml");
+		 * 
+		 * StudentDAO studentDAO = (StudentDAO) context.getBean("studentDAO");
+		 * Student student = new Student(); student.setName("lucy");
+		 * student.setSchool("beida"); studentDAO.addStudent(student); Student
+		 * student2 = new Student(); student2.setId(5L); student2.setName("Qi");
+		 * studentDAO.updateStudent(student2); studentDAO.delStudent(23L);
+		 * Student student1 = studentDAO.get(1L); System.out.println(student1);
+		 * List<Student> lS = null; lS = studentDAO.query(); Iterator iter =
+		 * lS.iterator(); while(iter.hasNext()){
+		 * System.out.println(iter.next());
+		 * 
+		 * } } catch (Exception e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
+	}
 }
